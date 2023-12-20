3f08d184c9407ccc7d59a48cf2ca72dd01d6c394
Your Name <you@example.com>
2023-12-21 00:59:53 +0300

    first commit

---
+++ project/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <HeaderComponent
+      v-if="$route.name != 'login' && $route.name != '404'"
+    ></HeaderComponent>
+    <router-view></router-view>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import HeaderComponent from "./components/layout/HeaderComponent.vue";
+// import "@/assets/style/global.scss";
+export default {
+  name: "app",
+  components: {
+    HeaderComponent,
+  },
+};
+</script>
+
+<style></style>
